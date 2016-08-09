c576285942af2daabaf78e8def6d6514eee7d9c8
shule517 <shule517@gmail.com>
2016-08-09 23:08:27 +0900

    asnc awaitに対応(babel-stage3)

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@ var _person2 = _interopRequireDefault(_person);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
 const jake = new _person2.default('Jake');
 const non = new _person2.default();
 
@@ -13,4 +15,28 @@ jake.speak();
 non.speak();
 
 console.log('test');
+
+class Test {
+    sleep(msec, val) {
+        console.log('sleep(' + val + ')');
+        return new Promise(function (resolve, reject) {
+            setTimeout(resolve, msec, val);
+        });
+    }
+
+    main() {
+        var _this = this;
+
+        return _asyncToGenerator(function* () {
+            console.log('----- start ----');
+            let result1 = yield _this.sleep(3000, 'a1');
+            console.log('result1 = ' + result1);
+            let result2 = yield _this.sleep(3000, 'a2');
+            console.log('result2 = ' + result2);
+            console.log('----- end -----');
+        })();
+    }
+}
+var test = new Test();
+test.main();
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -9,3 +9,23 @@ jake.speak();
 non.speak();
 
 console.log('test');
+
+class Test {
+    sleep(msec, val) {
+        console.log('sleep(' + val + ')');
+        return new Promise(function (resolve, reject) {
+            setTimeout(resolve, msec, val);
+        });
+    }
+
+    async main() {
+        console.log('----- start ----');
+        let result1 = await this.sleep(3000, 'a1');
+        console.log('result1 = ' + result1);
+        let result2 = await this.sleep(3000, 'a2');
+        console.log('result2 = ' + result2);
+        console.log('----- end -----');
+    }
+}
+var test = new Test();
+test.main();
